6d4288d19dd6ea4f79c22e59c53e37b3773efb2b
aetana <amanuel.etanan@gmail.com>
2023-02-07 23:45:56 -0600

    blog update1

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal} from 'react-bootstrap';
 
 const Blog = ({ data }) => {
 
